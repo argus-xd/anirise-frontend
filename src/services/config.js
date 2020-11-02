cc3deeb8a060ed75c3ac1becc3e93e0fa0347fa3
CodeSprut <Alex-p.s@yandex.ru>
2020-11-03 01:05:14 +0500

    backend host env variable typo fixed

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default () => {
   return axios.create({
-    baseURL: process.env.BACKEND_HOST || "http://127.0.0.1:8090",
+    baseURL: process.env.VUE_APP_BACKEND_HOST || "http://127.0.0.1:8090",
   });
 };
