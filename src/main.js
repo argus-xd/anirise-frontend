6ac3c51d696ed54b8f54d7b020991079a14d19c2
CodeSprut <Alex-p.s@yandex.ru>
2020-11-05 23:17:39 +0500

    css framework changed to materialize

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import app from "./app.vue";
 import router from "./router";
 
-import "bootstrap/dist/css/bootstrap.css";
+import "materialize-css";
+import 'materialize-css/dist/css/materialize.min.css'
 
 createApp(app).use(router).mount("#app");
