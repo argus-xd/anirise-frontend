0afb3ab7773880ccd189f8f5becf634ce37f4ab4
CodeSprut <Alex-p.s@yandex.ru>
2020-11-05 23:51:44 +0500

    overpass and roboto fonts added

--- src/app.vue
+++ src/app.vue
@@ -14,11 +14,4 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-
-  margin-top: 60px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import app from "./app.vue";
 import router from "./router";
 
 import "materialize-css";
-import 'materialize-css/dist/css/materialize.min.css'
+import "materialize-css/dist/css/materialize.min.css";
+import "./assets/styles/default.scss";
 
 createApp(app).use(router).mount("#app");
