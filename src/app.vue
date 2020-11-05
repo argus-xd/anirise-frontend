0339b59c974ca57a079d2e54f20247c851b1c574
CodeSprut <Alex-p.s@yandex.ru>
2020-11-06 01:11:19 +0500

    footer style

--- src/app.vue
+++ src/app.vue
@@ -9,6 +9,34 @@
   <div class="container">
     <router-view />
   </div>
+  <footer class="page-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5>AnimeRise</h5>
+          <p>Complete and completely free anime database with online player.</p>
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <h5>Links</h5>
+          <ul>
+            <li><a href="#">First</a></li>
+            <li><a href="#">Another</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+        © Powered by Kodik
+        <a
+          class="right"
+          target="_blank"
+          href="https://github.com/argus-xd/anirise-frontend"
+          >SourceCode</a
+        >
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
@@ -26,6 +54,13 @@ nav {
     font-family: var(--font-family-overpass);
     font-size: 1.8rem;
     font-weight: 600;
+    color: #fff;
   }
+}
+
+.page-footer {
+  background-color: #2b2d42;
+  color: #bcbedc;
+  font-size: 1.2rem;
 }
 </style>
